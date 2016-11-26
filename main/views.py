07a5df822e8abef6174d9e4d8212100514419224
Carlos Rodríguez <carlos@beatbcn.com>
2016-11-26 18:37:07 +0200

    Station Display Views

--- main/views.py
+++ main/views.py
@@ -19,3 +19,13 @@ class StationViewSet(viewsets.ModelViewSet):
 class BusViewSet(viewsets.ModelViewSet):
     queryset = Bus.objects.all()
     serializer_class = BusSerializer
+
+
+class StationDisplayStaticView(DetailView):
+    model = Station
+    template_name = 'main/station_display_static.html'
+
+
+class StationDisplayDynamicView(DetailView):
+    model = Station
+    template_name = 'main/station_display_dynamic.html'
