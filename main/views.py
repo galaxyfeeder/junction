b64b0c813d3868f05f8cf77fd7315579d4bb35a7
Gabriel Esteban <gabriel.esteban.gullon@gmail.com>
2016-11-26 13:09:39 +0100

    Misc on viewsets

--- main/views.py
+++ main/views.py
@@ -4,11 +4,11 @@ from main.models import Station, Bus
 from main.serializers import StationSerializer, BusSerializer
 
 
-class StationViewSet(viewsets.ReadOnlyModelViewSet):
+class StationViewSet(viewsets.ModelViewSet):
     queryset = Station.objects.all()
     serializer_class = StationSerializer
 
 
-class BusViewSet(viewsets.ReadOnlyModelViewSet):
+class BusViewSet(viewsets.ModelViewSet):
     queryset = Bus.objects.all()
     serializer_class = BusSerializer
