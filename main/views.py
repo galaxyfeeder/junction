ca83db288f378e73b575b4e5f1eab00a1c553490
Carlos Rodríguez <carlos@beatbcn.com>
2016-11-26 14:11:27 +0200

    Settings + URLs + Linemap

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from main.models import Line
+
+
+class LineDetailView(DetailView):
+    model = Line
+
