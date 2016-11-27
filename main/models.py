1071c82990cbf8b3200ebec5d77b74d08873c498
Carlos Rodríguez <carlos@beatbcn.com>
2016-11-27 11:13:53 +0200

    Multiple changes

--- main/models.py
+++ main/models.py
@@ -28,6 +28,8 @@ class Stop(models.Model):
     station = models.ForeignKey(Station)
     line = models.ForeignKey(Line, related_name='stops')
     people_waiting = models.PositiveIntegerField(default=0)
+    people_leaving = models.PositiveIntegerField(default=0)
+    travel_time = models.PositiveIntegerField(default=0)
 
     class Meta:
         unique_together = (('line', 'station'), ('line', 'order'))
@@ -35,6 +37,26 @@ class Stop(models.Model):
 
     def __unicode__(self):
         return self.line.number + " " + str(self.order) + ": " + self.station.name
+
+    def _will_stop(self):
+        return not (self.people_waiting == 0 and self.people_leaving == 0)
+
+    will_stop=property(_will_stop)
+
+    def stop_time(self):
+        return 10 + max(self.people_waiting, self.people_leaving) * 3
+
+    def stopped(self):
+        self.people_waiting = 0
+        self.people_leaving = 0
+        self.save()
+
+    def destination_travel_time(self, destination_stop):
+        stops = Stop.objects.filter(line=self.line, order__gt=self, order__lte=destination_stop)
+        traveltime = 0
+        for stop in stops:
+            traveltime += stop.travel_time + stop.stop_time()
+        return traveltime
 
 
 class Bus(models.Model):
