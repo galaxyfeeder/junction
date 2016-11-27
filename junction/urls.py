b1bbe41006d0b88868ddc1831b219be857ff90ba
Gabriel Esteban <gabriel.esteban.gullon@gmail.com>
2016-11-27 11:09:26 +0100

    Added redirect view

--- junction/urls.py
+++ junction/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 from main.views import LineDetailView, StationDisplayDynamicView, StationDisplayStaticView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/api')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('main.api')),
